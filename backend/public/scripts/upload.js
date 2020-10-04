38aa10bf97174441d33c18d178864c6de369d5fa
vithushan_IT16041776 <jeyasothy.vithushan@my.sliit.lk>
2020-10-04 16:58:47 +0530

    fix: add changes to upload.js

--- backend/public/scripts/upload.js
+++ backend/public/scripts/upload.js
@@ -1,32 +1,42 @@
 function readURL(input) {
-    if (input.files && input.files[0]) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    console.log("image read");
 
-        var reader = new FileReader();
+    reader.onload = function (e) {
+      $(".image-upload-wrap").hide();
 
-        reader.onload = function (e) {
-            $('.image-upload-wrap').hide();
+      $(".file-upload-image").attr("src", e.target.result);
+      $(".file-upload-content").show();
+      $(".file-upload-image").show();
 
-            $('.file-upload-image').attr('src', e.target.result);
-            $('.file-upload-content').show();
+      $(".image-title").html(input.files[0].name);
+    };
 
-            $('.image-title').html(input.files[0].name);
-        };
-
-        reader.readAsDataURL(input.files[0]);
-
-    } else {
-        removeUpload();
-    }
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    removeUpload();
+  }
 }
 
 function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.file-upload-content').hide();
-    $('.image-upload-wrap').show();
+  $(".file-upload-input").replaceWith($(".file-upload-input").clone());
+  $(".file-upload-content").hide();
+  $(".file-upload-image").hide();
+  $(".image-upload-wrap").show();
 }
-$('.image-upload-wrap').bind('dragover', function () {
-    $('.image-upload-wrap').addClass('image-dropping');
+$(".image-upload-wrap").bind("dragover", function () {
+  $(".image-upload-wrap").addClass("image-dropping");
 });
-$('.image-upload-wrap').bind('dragleave', function () {
-    $('.image-upload-wrap').removeClass('image-dropping');
+$(".image-upload-wrap").bind("dragleave", function () {
+  $(".image-upload-wrap").removeClass("image-dropping");
+});
+
+function toggleUserPanel() {
+  $(".user-panel").toggle();
+}
+
+$(function () {
+  $(".file-upload-image").hide();
+  $(".user-panel").hide();
 });
